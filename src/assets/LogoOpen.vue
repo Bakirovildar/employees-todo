27968422f769bb9b7fec6e4bb2524f668f614542
Bakirovildar <bakirovildar2011@mail.ru>
2022-11-24 13:52:04 +0500

    first commit

---
+++ src/assets/LogoOpen.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="10" height="6" viewBox="0 0 10 6" fill="none">
+    <path d="M8.825 0L5 3.7085L1.175 0L0 1.1417L5 6L10 1.1417L8.825 0Z" fill="#128BE3"/>
+  </svg>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HomePage/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page-container">
+    <div class="left-board">
+      <LogoEmployees />
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import LogoEmployees from "@/assets/LogoEmployees";
+export default {
+  name: 'HomePage',
+  components: {LogoEmployees},
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.left-board {
+  height: 100vh;
+  width: 88px;
+  padding-top: 48px;
+  background: #FFFFFF;
+  border-right: 1px solid #DBDADA;
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
+}
+
+.page-container {
+  display: flex;
+}
+</style>

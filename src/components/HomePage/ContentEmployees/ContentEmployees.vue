c11a829afca6772ee638646362ff37b41fcf60ba
Bakirovildar <bakirovildar2011@mail.ru>
2022-11-26 14:32:59 +0500

    небольшой рефакторинг, перенес поиск к родителю

--- src/components/HomePage/ContentEmployees/ContentEmployees.vue
+++ src/components/HomePage/ContentEmployees/ContentEmployees.vue
@@ -2,7 +2,15 @@
   <div class="contentEmployees">
     <HeaderContent/>
     <ContentButtons />
-    <InputSearch/>
+    <div class="message-send-input">
+      <input
+          v-model="searchValue"
+          type="text"
+          placeholder="Поиск по ФИО"
+          style="color: #8C9EB4;"
+      >
+      <div class="message-send-input-add__file"><LogoSearch/></div>
+    </div>
     <ContenPage/>
   </div>
 </template>
@@ -10,11 +18,16 @@
 <script>
 import HeaderContent from "@/components/HomePage/ContentEmployees/Header/HeaderContent";
 import ContentButtons from "@/components/HomePage/ContentEmployees/ContentButtons/ContentButtons";
-import InputSearch from "@/components/general/InputSearch";
 import ContenPage from "@/components/HomePage/ContentEmployees/content/ContenPage";
+import LogoSearch from "@/assets/LogoSearch";
 
 export default {
-  components: {ContenPage, InputSearch, ContentButtons, HeaderContent}
+  components: {LogoSearch, ContenPage, ContentButtons, HeaderContent},
+  data() {
+    return {
+      searchValue: ''
+    }
+  }
 }
 </script>
 
@@ -22,5 +35,38 @@ export default {
 .contentEmployees {
   margin: 47px 100px;
   width: 100%;
+}
+
+.message-send-input {
+  margin-top: 30px;
+  margin-left: 100px;
+  background: #FFFFFF;
+  border: 1px solid #DBDADA;
+  box-shadow: inset 0px 1px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 1px;
+  display: flex;
+  justify-content: space-between;
+  height: 40px;
+  width: 304px;
+  align-items: center;
+}
+
+input {
+  height: 36px;
+  width: 100%;
+  border: none;
+  outline: none;
+  padding: 0 12px;
+}
+
+.message-send-input-add__file {
+  width: 28px;
+  height: 28px;
+  background: #FFFFFF;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 10px 0 4px;
 }
 </style>
